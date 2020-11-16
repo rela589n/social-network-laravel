128c38d6f4711008d08e3047aabc8d0bc31cf9af
rela589n <zsidelnik@gmail.com>
2020-11-16 21:40:33 +0200

    feat: use ide helper

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,52 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string|null $avatar
+ * @property string $username
+ * @property string $password
+ * @property string|null $first_name
+ * @property string|null $last_name
+ * @property string $gender
+ * @property string|null $location
+ * @property int $verify
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|User[] $friendOf
+ * @property-read int|null $friend_of_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|User[] $friendsOfMine
+ * @property-read int|null $friends_of_mine_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Like[] $likes
+ * @property-read int|null $likes_count
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Wall[] $walls
+ * @property-read int|null $walls_count
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereAvatar($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereGender($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLocation($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUsername($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereVerify($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
--- app/Models/Wall.php
+++ app/Models/Wall.php
@@ -4,6 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Wall
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int|null $parent_id
+ * @property string $body
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Like[] $likes
+ * @property-read int|null $likes_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|Wall[] $replies
+ * @property-read int|null $replies_count
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|Wall newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Wall newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Wall notReply()
+ * @method static \Illuminate\Database\Eloquent\Builder|Wall query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Wall whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Wall whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Wall whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Wall whereParentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Wall whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Wall whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Wall extends Model
 {
     protected $table = 'walls';
