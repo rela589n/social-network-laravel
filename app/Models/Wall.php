1fe056fb22e05992b77cddea36af4446498c0862
rela589n <zsidelnik@gmail.com>
2020-11-17 21:55:48 +0200

    cleanup(models): remove clutter

--- app/Models/Wall.php
+++ app/Models/Wall.php
@@ -35,25 +35,21 @@ class Wall extends Model
     protected $table = 'walls';
     protected $fillable = ['body'];
 
-    # получить пользователя по записи на стене (обратное отношение)
     public function user()
     {
         return $this->belongsTo('App\Models\User', 'user_id');
     }
 
-    # выбрать все родительские записи на стене
     public function scopeNotReply($query)
     {
         return $query->whereNull('parent_id');
     }
 
-    # получить комментарии к записи на стене (связь один ко многим)
     public function replies()
     {
         return $this->hasMany('App\Models\Wall', 'parent_id')->latest();
     }
 
-    # Получить все лайки к записи
     public function likes()
     {
         return $this->morphMany('App\Models\Like', 'likeable');
